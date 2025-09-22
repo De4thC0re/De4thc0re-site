c040fab1bcb51caf141c691bbd220a55a01f581c
De4thC0re <uwubakatv@gmail.com>
2025-09-22 10:12:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,59 @@
+// Loader
 window.addEventListener('load', ()=>{
     setTimeout(()=>{
-        const loader = document.getElementById('loader');
-        loader.classList.add('hide');
+        document.getElementById('loader').classList.add('hide');
         setTimeout(()=>{
             document.getElementById('content').style.display='block';
-            startTyping();
+            animateCounters();
+            initParticles();
         },1000);
-    },2000);
+    },1500);
 });
 
-const terminalEl = document.getElementById('terminal');
-const cursor = document.createElement('span');
-cursor.className='cursor';
-terminalEl.appendChild(cursor);
-
-// particles
-for(let i=0;i<50;i++){
-    const p=document.createElement('div');
-    p.className='particle';
-    p.style.left=Math.random()*100+'%';
-    p.style.top=Math.random()*100+'%';
-    p.style.animationDuration=(Math.random()*3+2)+'s';
-    p.style.width=p.style.height=(Math.random()*3+1)+'px';
-    terminalEl.appendChild(p);
+// Animowane liczniki (możesz podmienić na API)
+function animateCounters() {
+    const counters = [
+        {el: document.getElementById('commits'), target: 523},
+        {el: document.getElementById('repos'), target: 12},
+        {el: document.getElementById('followers'), target: 42}
+    ];
+    counters.forEach(counter => {
+        counter.el.innerText = '0';
+        const update = () => {
+            const current = +counter.el.innerText;
+            const increment = counter.target / 100;
+            if(current < counter.target){
+                counter.el.innerText = `${Math.ceil(current + increment)}`;
+                setTimeout(update, 20);
+            } else {
+                counter.el.innerText = counter.target;
+            }
+        };
+        update();
+    });
 }
 
-// kod w terminalu
-const codeLines=[
-    "console.log('Witaj, Świecie');",
-    "function add(a,b){",
-    "  return a+b;",
-    "}",
-    "console.log(add(5,7));",
-    "let x=10;",
-    "x+=5;",
-    "console.log(x);"
-];
-
-let lineIndex=0;
-
-function typeLine(line,callback){
-    let charIndex=0;
-    cursor.style.display='none';
-    const interval=setInterval(()=>{
-        terminalEl.innerHTML+=line[charIndex];
-        charIndex++;
-        if(charIndex===line.length){
-            clearInterval(interval);
-            terminalEl.innerHTML+="<br>";
-            cursor.style.display='inline-block';
-            if(callback) callback();
-        }
-    },50);
-}
-
-function typeNextLine(){
-    if(lineIndex<codeLines.length){
-        typeLine(codeLines[lineIndex],()=>{
-            lineIndex++;
-            setTimeout(typeNextLine,600);
-        });
+// Proste particles
+function initParticles(){
+    const container = document.getElementById('particles');
+    for(let i=0;i<50;i++){
+        const p = document.createElement('div');
+        p.className='particle';
+        p.style.left=Math.random()*100+'%';
+        p.style.top=Math.random()*100+'%';
+        p.style.width=p.style.height=(Math.random()*3+1)+'px';
+        p.style.animationDuration=(Math.random()*3+2)+'s';
+        container.appendChild(p);
     }
 }
 
-function startTyping(){
-    lineIndex=0;
-    terminalEl.innerHTML="";
-    terminalEl.appendChild(cursor);
-    typeNextLine();
-}
+// --- opcjonalnie: dynamiczne pobieranie danych z GitHub API ---
+/*
+fetch('https://api.github.com/users/twojNick')
+  .then(res => res.json())
+  .then(data => {
+      document.getElementById('repos').dataset.target = data.public_repos;
+      document.getElementById('followers').dataset.target = data.followers;
+      animateCounters();
+  });
+*/
